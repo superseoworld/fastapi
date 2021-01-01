8a7297386567eb5604abff65da35905f837a4ddd
Thomas Wawra <twawra@gmail.com>
2021-01-01 13:17:45 +0100

    - refac status -> status_code

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 
 app = FastAPI()
 
+
 # domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
 
@@ -15,13 +16,12 @@ def home():
 @app.get("/get_content/")
 def get_content(url: str):
     url_valid = uri_exists_stream(url)
-    if url_valid == True:
+    if url_valid:
         extractor = extractors.KeepEverythingExtractor()
         try:
             doc = extractor.get_content_from_url(url)
             return {"content": doc}
         except:
-            print("An exception occurred")
             pass
     else:
         return {'msg': url_valid}
@@ -34,6 +34,6 @@ def uri_exists_stream(uri: str) -> bool:
                 response.raise_for_status()
                 return True
             except requests.exceptions.HTTPError as err:
-                return {'err': err, 'status': err.response.status_code}
+                return {'err': err, 'status_code': err.response.status_code}
     except requests.exceptions.ConnectionError as err:
-        return {'err': err, 'status': err.response.status_code}
+        return {'err': err, 'status_code': err.response.status_code}
