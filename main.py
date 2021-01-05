4c49de2f40048cd7ccd38f3bf161015e95e45f99
Thomas Wawra <twawra@gmail.com>
2021-01-05 12:06:21 +0100

    - test spacy

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def validate_url(url: str):
 
 @app.get("/get_entities/")
 def get_entities(url: str):
-    nlp = en_core_web_sm.load()
+    # nlp = en_core_web_sm.load()
     doc = get_content(url)
     return None
 
