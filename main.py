8978b48e07d2bb415a1354d4c537268bcc3b58aa
Thomas Wawra <twawra@gmail.com>
2021-01-05 18:18:20 +0100

    - test spacy

--- main.py
+++ main.py
@@ -73,10 +73,9 @@ def validate_url(url: str):
 @app.get("/get_entities/")
 def get_entities(url: str):
     nlp = en_core_web_sm.load()
-    doc = get_content(url)
-    doc = nlp(doc.get('content'))
-    doc = {'ents': [str(ent) for ent in doc.ents]}
-    doc = pd.DataFrame(doc)
-    return {'msg': doc}
+    content = get_content(url)
+    doc = nlp(content.get('content'))
+
+    return {'msg': content}
 
 
